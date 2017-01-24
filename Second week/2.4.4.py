2a3bf81b8e7cd0ba29b11839e49c2317dc25f903
Workstation_4 <ahil52_25@mail.ru>
2017-01-24 14:50:48 +0600

    complite

---
+++ Second week/2.4.4.py
@@ -0,0 +1,10 @@
+def main():
+    lines = []
+    with open("dataset_24465_4.txt", "r") as inp:
+        lines = [line for line in inp]
+
+    with open("output.txt", "w") as out:
+        out.writelines(reversed(lines))
+
+if __name__ == "__main__":
+    main()
